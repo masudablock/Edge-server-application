fa1c0ff74d3a3159a67a7385a9392bd96c5e209d
masudablock <77135332+masudablock@users.noreply.github.com>
2021-01-08 12:42:15 +0900

    Create determine_execute.py

---
+++ determine_execute.py
@@ -0,0 +1,19 @@
+# json形式で出力。
+# @parameter:
+#   camera_id: カメラ番号
+#   count:それぞれのデバイスの書き込み待機数
+# @return:
+#   実行するかどうかの判定
+def determine_execute(camera_id,count,importance,N):
+        judge = 0
+        max_index = 0
+        for i in range(1,N+1):
+                if judge > count[i] / importance[i]:
+                        judge = count[i] / importance[i]
+                        max_index = i
+        if count[camera_id] > 1000:
+                return 1
+        elif max_index == camera_id:
+                return 1
+        else:
+                return 0
